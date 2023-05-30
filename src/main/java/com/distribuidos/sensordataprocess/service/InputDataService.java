5821da7d27847a1b380743bdec20a17baf735640
albornozmartin <martin.albornoz@correo.ucu.edu.uy>
2023-05-30 20:10:40 -0300

    bd
    cambios en las properties, pom y inputdataservice

--- src/main/java/com/distribuidos/sensordataprocess/service/InputDataService.java
+++ src/main/java/com/distribuidos/sensordataprocess/service/InputDataService.java
@@ -6,6 +6,8 @@ import com.distribuidos.sensordataprocess.repository.DataRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.*;
+
 @Service
 @AllArgsConstructor
 public class InputDataService {
@@ -15,6 +17,8 @@ public class InputDataService {
     private final AuthService authService;
 
     private final MessageService messageService;
+
+
 
     public SensorPostDataResponse sendData(InputSensorData inputSensorData) {
         if (!validateData(inputSensorData)) {
@@ -34,4 +38,33 @@ public class InputDataService {
         var validtimeStamp = inputSensorData.getTimeStamp() != null;
         return validSensor && validWaterFlow && validtimeStamp;
     }
+
+
+
+
+    public void saveData(InputSensorData data) {
+        // abro coenxion
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/sensorData", "distribuidosAdmin", "K3DsTQ3RxuC4b4");
+
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO input_sensor_data (sensor_id, latitude, longitude, area, timestamp_value, water_flow) VALUES (?, ?, ?, ?, ?, ?)");
+
+            // seteo parametros
+            statement.setLong(1, data.getSensorId());
+            statement.setDouble(2, data.getLocation().getLatitude());
+            statement.setDouble(3, data.getLocation().getLongitude());
+            statement.setString(4, data.getArea());
+            statement.setLong(5, data.getTimeStamp());
+            statement.setDouble(6, data.getWaterFlow());
+
+            statement.executeUpdate();
+
+            // cierro con
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
